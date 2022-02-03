3da9d3a50aea332cd6b60b0d5e4acfc120830b5c
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-02-03 14:40:20 +0530

    storage class

---
+++ C INTRODUCTION/my.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void fun();
+
+void main(){
+    fun();
+    fun();
+    fun();
+}
+
+void fun(){
+    static int x=1;
+    int y=1;
+    x++;
+    y++;
+    printf("Y = %d\n",y);
+    printf("X = %d\n",x);
+}
